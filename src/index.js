982811ec2d93beeee348f6b57bfb04bc40330c10
Vitor R. Bizzi <vitorbizzi5@gmail.com>
2020-07-09 04:21:24 -0300

    Install modules 'nodemon'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require ('express');
+const { response } = require('express');
 const server = express ();
+
+server.get('/teste', (req, resp) =>{
+    resp.send('Mudei a API');
+});
 
 server.listen (3000, ()  => {
     console.log('API ONLINE');
